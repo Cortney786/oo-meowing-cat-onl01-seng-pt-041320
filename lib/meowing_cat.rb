6ba5884c9e7d06a9e8e6c3709573f5d75e0cd3a3
Cortney Dawkins <cmddawkinscortney@gmail.com>
2020-04-27 21:29:56 +0000

    Automatically backed up by Learn

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -9,22 +9,19 @@ def cat
   @cat 
 end 
 
+class Cat 
+  attr_writer :cat 
+  attr_reader :cat 
+  
+end 
 
-
-
-
-
-
-
-
-
-class Person
-  attr_writer :name
-  attr_reader :name
- 
+class Cat 
+  attr_accessor :cat 
 end
 
-class Person
-  attr_accessor :name
- 
-end
+
+
+
+
+
+
